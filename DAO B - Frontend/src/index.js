0862e9045553e2df7e01df60f1c9fed2003ffbf6
Kareem Mostafa <kmostafa599@gmail.com>
2022-06-13 08:18:08 +0200

    debugging issue

--- DAO B - Frontend/src/index.js
+++ DAO B - Frontend/src/index.js
@@ -13,7 +13,7 @@ import { compose } from "redux";
 
 const middleware = [thunk]
 const store = createStore(reducers,compose(applyMiddleware(...middleware),
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
 ReactDOM.render(
